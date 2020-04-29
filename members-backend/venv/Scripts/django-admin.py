2fec3c338bee0da27fcc0787301ba21fd07a9a02
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-04-29 20:10:18 -0300

    5. Preparacão do ambiente Angular no

---
+++ members-backend/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\dev\estudos\angular-crud\members\members-backend\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
